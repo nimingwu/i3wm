983a257413ab098012199e37d6e13a5b358aa5c3
iluwaf <106297884+nimingwu@users.noreply.github.com>
2025-03-26 22:38:03 +0800

    Create proxy.sh

---
+++ proxy.sh
@@ -0,0 +1,7 @@
+sudo mkdir /run/systemd/system/nix-daemon.service.d/
+cat << EOF >/run/systemd/system/nix-daemon.service.d/override.conf
+[Service]
+Environment="https_proxy=socks5h://localhost:7891"
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart nix-daemon
